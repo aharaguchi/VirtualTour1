50941e3b4d7365c107ee3b8b0f9609dc4b27c2bd
Akemi <akharaguchi@gmail.com>
2017-04-13 16:42:21 -0700

    fixed an error in sitemap.php

--- Master/sitemap.php
+++ Master/sitemap.php
@@ -6,7 +6,7 @@
 <?php include("ProtoNavLogged.php"); ?>
 
 	<div id="mainContent">  <!-- Insert your part of the website here. -->
-			<h2 id="mapTitle">Virtual Tour - Site Map<h1>
+			<h2 id="mapTitle">Virtual Tour - Site Map</h2>
 			<h3 class="mapHeading"><a href="index.php">Home<br></a></h3>
 			<h3 class="mapHeading"><a href="allbuild.php">Buildings</a></h3>
 			<h4 class="mapCategory"><a href ="eachbuildNE1.php">Northeast 1 Building</a></h4>
