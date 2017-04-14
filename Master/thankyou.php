5af66fce132a9340cf2cd47ea58b780bbdaa3f15
Simon Wu <swu111@my.bcit.ca>
2017-04-13 17:10:55 -0700

    made thankyou.php

---
+++ Master/thankyou.php
@@ -0,0 +1,12 @@
+<?php include("ProtoHeader.php"); ?>
+<!--Page-specific <title>-->
+<title>Virtual Tour SiteMap</title>
+<!--Page-specific <js and css>-->
+<link rel="stylesheet" href="css/sitemap.css" media="screen">
+<?php include("ProtoNavLogged.php"); ?>
+
+	<div id="mainContent">  <!-- Insert your part of the website here. -->
+			<h1>Thank you! You're comments are submitted!</h1>
+		</div>  <!--End of mainContent-->
+
+<?php include ("ProtoFooter.php"); ?>
